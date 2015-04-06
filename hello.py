194cf995218e1c3dff737be6c6d05acd3d4e675d
Erdong Feng <edfeng@thoughtworks.com>
2015-04-06 17:57:22 +0800

    save and query data with flask-alchemy.

--- hello.py
+++ hello.py
@@ -1,12 +1,46 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+import os
+
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.script import Manager, Shell
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+
 __author__ = 'edfeng'
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = """3z&tlt+9_6cj+l69jnva(y*=7--2r)_j%kv6!+key6mb+6xxu5"""
+basedir = os.path.abspath(os.path.dirname(__name__))
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    # has many users, also insert a 'role' attribute to User model, it can be used to access related role from a user
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    # foreign key
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(Form):
@@ -18,16 +52,30 @@ class NameForm(Form):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash("Looks like you've changed your name")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
     else:
-        return render_template('index.html', form=form, name=session.get('name'))
+        return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 
+def make_shell_context():
+    """
+    Import database instance and models
+    :return:
+    """
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+Bootstrap(app)
 if __name__ == '__main__':
-    Bootstrap(app)
-    app.config['SECRET_KEY'] = """3z&tlt+9_6cj+l69jnva(y*=7--2r)_j%kv6!+key6mb+6xxu5"""
-    app.run(debug=True)
+    manager = Manager(app)
+    manager.add_command('shell', Shell(make_context=make_shell_context))
+    manager.run()
